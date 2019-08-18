568e7a09e81744e1e6c27f4ad6603dc019df8219
chuck1193 <>
2019-08-18 16:44:36 -0500

    Created the different game pieces and randomly selected each.

--- tetris.js
+++ tetris.js
@@ -4,11 +4,6 @@ const context = canvas.getContext('2d');
 context.scale(20, 20);
 
 
-const matrix = [
-  [0, 0, 0],
-  [1, 1, 1],
-  [0, 1, 0],
-];
 
 function collide(arena, player){
   const [m, o] = [player.matrix, player.pos];
@@ -32,8 +27,50 @@ function createMatrix(width, height) {
   return matrix;
 }
 
-function createPiece(){
-  
+function createPiece(type){
+  if (type === 'T') {
+    return [
+            [0, 0, 0],
+            [1, 1, 1],
+            [0, 1, 0],
+            ];
+  } else if (type === 'O') {
+      return [
+              [1, 1],
+              [1, 1],
+            ];
+  } else if (type === 'L') {
+      return [
+            [0, 1, 0],
+            [0, 1, 0],
+            [0, 1, 1],
+            ];
+  } else if (type === 'I') {
+    return [
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+            ];
+  } else if (type === 'J') {
+    return [
+            [0, 1, 0],
+            [0, 1, 0],
+            [1, 1, 0],
+            ];
+  } else if (type === 'S') {
+    return [
+            [0, 1, 1],
+            [1, 1, 0],
+            [0, 0, 0],
+            ];
+  } else if (type === 'Z') {
+    return [
+            [1, 1, 0],
+            [0, 1, 1],
+            [0, 0, 0],
+            ];
+  }
 }
 
 function draw() {
@@ -72,7 +109,7 @@ function playerDrop() {
   if (collide(arena, player)) {
     player.pos.y--;
     merge(arena, player);
-    player.pos.y = 0;
+    playerReset();
   }
   dropCounter = 0;
 }
@@ -82,6 +119,14 @@ function playerMove(dir){
   if (collide(arena, player)){
     player.pos.x -= dir;
   }
+}
+
+function playerReset() {
+  const pieces = 'ILJOTSZ';
+  player.matrix = createPiece(pieces[pieces.length * Math.random() | 0]);
+  player.pos.y = 0;
+  player.pos.x = (arena[0].length / 2 | 0) - 
+                 (player.matrix[0].length / 2 | 0);
 }
 
 function playerRotate(dir){
@@ -138,7 +183,7 @@ const arena = createMatrix(12, 20);
 
 const player = {
   pos: {x: 5, y: 5},
-  matrix: matrix,
+  matrix: createPiece('T'),
 }
 
 document.addEventListener('keydown', event => {
