2bfa7c27c5b2542cad15288b2fc87da6ca681964
chuck1193 <>
2019-08-18 16:57:42 -0500

    Gave the pieces some color

--- tetris.js
+++ tetris.js
@@ -3,7 +3,9 @@ const context = canvas.getContext('2d');
 
 context.scale(20, 20);
 
-
+function arenaSweep(){
+  
+}
 
 function collide(arena, player){
   const [m, o] = [player.matrix, player.pos];
@@ -36,38 +38,38 @@ function createPiece(type){
             ];
   } else if (type === 'O') {
       return [
-              [1, 1],
-              [1, 1],
+              [2, 2],
+              [2, 2],
             ];
   } else if (type === 'L') {
       return [
-            [0, 1, 0],
-            [0, 1, 0],
-            [0, 1, 1],
+            [0, 3, 0],
+            [0, 3, 0],
+            [0, 3, 3],
             ];
   } else if (type === 'I') {
     return [
-            [0, 1, 0, 0],
-            [0, 1, 0, 0],
-            [0, 1, 0, 0],
-            [0, 1, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
             ];
   } else if (type === 'J') {
     return [
-            [0, 1, 0],
-            [0, 1, 0],
-            [1, 1, 0],
+            [0, 4, 0],
+            [0, 4, 0],
+            [4, 4, 0],
             ];
   } else if (type === 'S') {
     return [
-            [0, 1, 1],
-            [1, 1, 0],
+            [0, 6, 6],
+            [6, 6, 0],
             [0, 0, 0],
             ];
   } else if (type === 'Z') {
     return [
-            [1, 1, 0],
-            [0, 1, 1],
+            [7, 7, 0],
+            [0, 7, 7],
             [0, 0, 0],
             ];
   }
@@ -84,7 +86,7 @@ function drawMatrix(matrix, offset) {
   matrix.forEach((row, y) => {
     row.forEach((value, x) => {
        if(value !== 0) {
-        context.fillStyle = 'red';
+        context.fillStyle = colors[value];
         context.fillRect(x + offset.x, 
                          y + offset.y, 
                          1, 
@@ -127,6 +129,9 @@ function playerReset() {
   player.pos.y = 0;
   player.pos.x = (arena[0].length / 2 | 0) - 
                  (player.matrix[0].length / 2 | 0);
+  if (collide(arena, player)) {
+    arena.forEach(row => row.fill(0));
+  }
 }
 
 function playerRotate(dir){
@@ -179,6 +184,16 @@ function update(time = 0) {
   requestAnimationFrame(update);
 }
 
+const colors = [
+                null,
+                '#FF0D72',
+                '#0DC2FF',
+                '#ODFF72',
+                '#F538FF',
+                '#FF8E0D',
+                '#FFE138',
+                '#3877FF',
+          ];
 const arena = createMatrix(12, 20);
 
 const player = {
